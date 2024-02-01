9fa9261f12352b770fd9a382fa70490f9168fb41
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-02-01 12:52:02 +0530

    Bottom Navigation Bar

--- app/src/main/java/com/ashutosh/openinapp/Screens/LinkScreen.kt
+++ app/src/main/java/com/ashutosh/openinapp/Screens/LinkScreen.kt
@@ -1,6 +1,7 @@
 package com.ashutosh.openinapp.Screens
 
 import android.app.Activity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,11 +10,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,10 +32,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ashutosh.openinapp.R
+
+
+//
 
 @Composable
 fun LinkScreen(navController: NavHostController , activity: Activity) {
@@ -40,22 +50,25 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
             .fillMaxSize()
             .background(
                 Color(0xFF0E6FFF)
-            ),
-        verticalArrangement = Arrangement.Center,
+            )
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center ,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Spacer(modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        )
 
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp)
-                ,
-          horizontalArrangement = Arrangement.SpaceBetween,
+                .padding(horizontal = 20.dp) ,
+            horizontalArrangement = Arrangement.SpaceBetween ,
             verticalAlignment = Alignment.CenterVertically
         ) {
 
@@ -65,7 +78,7 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
                     .wrapContentWidth() ,
                 color = Color.White ,
                 textAlign = TextAlign.Center ,
-                fontSize = 22.sp ,
+                fontSize = 27.sp ,
                 fontWeight = FontWeight.ExtraBold
             )
 
@@ -75,7 +88,7 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
                     .width(50.dp)
                     .height(50.dp)
                     .clip(RoundedCornerShape(20.dp))
-                    .background(Color.White),
+                    .background(Color.White) ,
                 verticalArrangement = Arrangement.Center ,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -87,6 +100,13 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
         }
 
 
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+        )
+
+
 
 
         Column(
@@ -95,19 +115,153 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
                 .clip(RoundedCornerShape(topEnd = 30.dp , topStart = 30.dp))
                 .background(
                     Color.White
-                ),
-            verticalArrangement = Arrangement.Center,
+                ) ,
+            verticalArrangement = Arrangement.Top ,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            )
 
+            GreetingSection()
+
+            GraphSection()
+
+            ModelDataList()
+
+
+            ViewAnalytics()
+
+            LinkSection()
 
         }
+    }
+}
+
+@Composable
+fun LinkSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(600.dp)
+            .padding(20.dp)
+            .background(
+                Color.Cyan
+            ) ,
+        verticalArrangement = Arrangement.Top ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
 
 
+    }
+}
+
+@Composable
+fun ViewAnalytics() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .padding(20.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                Color.Green
+            ) ,
+        colors = CardDefaults.cardColors(Color.White),
+        border = BorderStroke(1.dp,Color.Gray)
+    ) {
 
 
 
-        }
+    }
+
+}
+
+@Composable
+fun ModelDataList() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(20.dp)
+            .background(
+                Color.Blue
+            ) ,
+        verticalArrangement = Arrangement.Top ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+
+    }
+
+}
+
+
+@Composable
+fun GraphSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(20.dp)
+            .clip(RoundedCornerShape(topEnd = 30.dp , topStart = 30.dp))
+            .background(
+                Color.Yellow
+            ) ,
+        verticalArrangement = Arrangement.Top ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+
+    }
+
+}
+
+
+@Composable
+fun GreetingSection() {
+    Text(
+        text = "Good Morning" ,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 20.dp),
+        color = Color.Gray ,
+        textAlign = TextAlign.Start ,
+        fontSize = 22.sp ,
+        fontWeight = FontWeight.Bold
+    )
+
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(horizontal = 20.dp) ,
+        horizontalArrangement = Arrangement.Start ,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            text = "Ajay Manva" ,
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(end = 10.dp),
+            color = Color.Black ,
+            textAlign = TextAlign.Start ,
+            fontSize = 26.sp ,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+        Icon(painter = painterResource(id = R.drawable.mail) , contentDescription = "ok",
+            tint = Color.Black)
+
+
+    }
 }
