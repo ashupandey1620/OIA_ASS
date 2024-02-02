e79cb2f7275595c7df1249af32b87fc33b15794b
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-02-02 16:58:22 +0530

    Rest Files

--- app/src/main/java/com/ashutosh/openinapp/Screens/LinkScreen.kt
+++ app/src/main/java/com/ashutosh/openinapp/Screens/LinkScreen.kt
@@ -18,42 +18,33 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.DefaultAlpha
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -73,7 +64,6 @@ import co.yml.charts.ui.linechart.model.Line
 import co.yml.charts.ui.linechart.model.LineChartData
 import co.yml.charts.ui.linechart.model.LinePlotData
 import co.yml.charts.ui.linechart.model.LineStyle
-import co.yml.charts.ui.linechart.model.LineType
 import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
 import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
 import co.yml.charts.ui.linechart.model.ShadowUnderLine
@@ -90,6 +80,26 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
 
     val linkScreenVM : GetLinkDataPageVM = hiltViewModel()
 
+    val responseData by linkScreenVM.getData.observeAsState()
+
+    val todayClicks = remember { mutableStateOf("") }
+    val topLocation = remember { mutableStateOf("") }
+    val topSocial = remember { mutableStateOf("") }
+    val topTime = remember { mutableStateOf("") }
+
+
+
+
+
+
+    responseData?.let { response ->
+        todayClicks.value = response.today_clicks.toString()
+        topLocation.value = response.top_location
+        topSocial.value = response.top_source
+        topTime.value = response.startTime
+
+
+        }
 
     LaunchedEffect(Unit){
         linkScreenVM.getLinkData()
@@ -182,7 +192,10 @@ fun LinkScreen(navController: NavHostController , activity: Activity) {
 
             GraphSection()
 
-            ModelDataList()
+            ModelDataList(todayClicks.value.toString(),
+                topLocation.value.toString(),
+                topSocial.value.toString(),
+                topTime.value.toString())
 
             ViewAnalytics()
 
@@ -474,7 +487,7 @@ fun ViewAnalytics() {
 }
 
 @Composable
-fun ModelDataList() {
+fun ModelDataList(clicks: String , location : String , social: String , time: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -487,19 +500,16 @@ fun ModelDataList() {
     ) {
 
 
-        var clicks = "293"
-        var topLocation = "Kanpur"
-        var social  = "Instagram"
-        var bestTime = "Best Time"
+
 
         Row(modifier = Modifier
             .fillMaxSize()
             .horizontalScroll(rememberScrollState())) {
 
             ModelDataListItem(R.drawable.click, clicks,   "Todays Click")
-            ModelDataListItem(R.drawable.location, topLocation,"Top Location")
+            ModelDataListItem(R.drawable.location, location,"Top Location")
             ModelDataListItem(R.drawable.social, social,  "Top Source")
-            ModelDataListItem(R.drawable.click, bestTime,   "Best Time")
+            ModelDataListItem(R.drawable.click, time,   "Best Time")
 
         }
 
